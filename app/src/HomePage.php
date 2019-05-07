6660853d649bcd80c3d1b0d6c9ebc8f9254dc6c0
Robbozinoz <Robbozinoz>
2019-05-07 11:53:17 +0800

    Installation of new HomePage class with new layouts for default and Homepage

---
+++ app/src/HomePage.php
@@ -0,0 +1,12 @@
+<?php
+//Tutorial 4 setup of class for new page template
+//namespace SilverStripe\Lessons;
+
+namespace SilverStripe\Onering_jr;
+
+use Page;    
+
+class HomePage extends Page 
+{
+
+}
---
+++ app/src/HomePageController.php
@@ -0,0 +1,13 @@
+<?php
+//Tutorial 4 setup of class for new page template
+//namespace SilverStripe\Lessons;
+
+namespace SilverStripe\Onering_jr;
+
+
+use PageController;    
+
+class HomePageController extends PageController 
+{
+
+}
